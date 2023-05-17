66694f0c54895b34aa5f15adf694f55046f6653c
Murat Yildirim <murat.yildirim@zvoove.com>
2023-05-17 17:22:48 +0200

    create new migration file for creating posts

---
+++ console/migrations/m230517_151924_create_post_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ */
+class m230517_151924_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%post}}');
+    }
+}
